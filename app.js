e252fe3c8a24237f31d164f84eb326b20a3b320b
jmj <wjsdncl2222@gmail.com>
2024-09-23 23:57:20 +0900

    Feat : CORS 설정 추가

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://wjsdncl-dev-hub.vercel.app/"],
+    credentials: true,
   })
 );
 app.use(express.json());
