cf4e5aacdde7be65932f94bc2474282d04952a63
jmj <wjsdncl2222@gmail.com>
2024-11-20 11:36:31 +0900

    Feat : 프로젝트 상세 내용 추가

--- prisma/migrations/20241119092123_add_content_to_project/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `content` to the `Project` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "content" TEXT NOT NULL;
---
+++ prisma/migrations/20241120023557_add_project_content/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "content" TEXT NOT NULL DEFAULT '';
