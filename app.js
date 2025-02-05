adfeca939d091f9438baab678350800e1f234697
jmj <wjsdncl2222@gmail.com>
2025-02-05 18:11:59 +0900

    Feat: CORS origin 추가

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "https://localhost:3000",
       "https://wjsdncl-dev-hub.vercel.app",
       "https://www.wjdalswo-dev.xyz",
     ],
@@ -64,14 +65,14 @@ function generateTokens(userId) {
 
 // 유저 인증 미들웨어
 function requiredAuthenticate(req, res, next) {
-  const { accessToken: token } = req.cookies;
+  const token = req.cookies?.accessToken;
   if (!token) return res.status(401).send({ message: "로그인이 필요합니다." });
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       // 액세스 토큰이 만료된 경우
       if (err.name === "TokenExpiredError") {
-        const { refreshToken } = req.cookies;
+        const refreshToken = req.cookies?.refreshToken;
         if (!refreshToken) {
           return res.status(401).send({ message: "세션이 만료되었습니다. 재로그인 해주세요." });
         }
@@ -112,7 +113,7 @@ function requiredAuthenticate(req, res, next) {
 
 // 로그인 선택 미들웨어
 function optionalAuthenticate(req, res, next) {
-  const { accessToken: token } = req.cookies;
+  const token = req.cookies?.accessToken;
   if (!token) {
     req.user = null;
     return next();
